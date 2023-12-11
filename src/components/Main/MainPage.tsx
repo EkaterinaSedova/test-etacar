c91b0c6f84383dabcab971384f46587ab56ac49a
Екатерина Седова <katena.sedova@gmail.com>
2023-12-11 22:31:56 +0300

    the search is placed in a separate component

--- src/components/Main/MainPage.tsx
+++ src/components/Main/MainPage.tsx
@@ -2,17 +2,13 @@ import React, { useEffect, useState } from 'react'
 import CryptoList from '../Cryptos/CryptoList'
 import { cryptosAPI } from '../../services/cryptosService'
 import styles from '../../Index.module.scss'
-import { CRYPTO_ROUTE } from '../../routing/paths'
-import { useNavigate } from 'react-router-dom'
 import Header from '../Header/Header'
 import Modal from '../Modals/Modal'
+import Search from "../Search/Search";
 const MainPage = () => {
   const limit = 100
-  const navigate = useNavigate()
   const [page, setPage] = useState(1)
   const [offset, setOffset] = useState(0)
-  const [searchValue, setSearchValue] = useState('')
-  const { data } = cryptosAPI.useFetchCryptosQuery(searchValue)
   const {
     data: cryptos,
     error,
@@ -22,12 +18,6 @@ const MainPage = () => {
   const isAvailable = () => {
     if (cryptos && cryptos.data.length < limit) return false
     else return true
-  }
-  const handleSearch = ({ target: { value } }: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchValue(value)
-  }
-  const handleSearchClick = (id: string) => {
-    navigate(CRYPTO_ROUTE + `/${id}`)
   }
   const handleNextClick = () => {
     setPage(page + 1)
@@ -51,34 +41,7 @@ const MainPage = () => {
         <>Loading...</>
       ) : cryptos ? (
         <>
-          <div className={styles.searchContainer}>
-            <input
-              type="search"
-              name="search"
-              placeholder="Search..."
-              autoComplete="off"
-              onChange={handleSearch}
-              value={searchValue}
-            />
-            {searchValue && (
-              <div className={styles.searchField}>
-                {data && data.data.length ? (
-                  data.data.map((item) => (
-                    <p
-                      key={item.id}
-                      onClick={() => {
-                        handleSearchClick(item.id)
-                      }}
-                    >
-                      {item.name}
-                    </p>
-                  ))
-                ) : (
-                  <p>No results.</p>
-                )}
-              </div>
-            )}
-          </div>
+          <Search />
           <CryptoList items={[...cryptos.data]} />
           {page > 1 ? (
             <button onClick={() => handlePrevClick()}>prev</button>
